c43a9240f13b11ed66914aad260f40ed1160bef4
Max-IT-ZT <babuskomax@gmail.com>
2023-08-02 21:55:50 +0300

    first-fix

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 // Оголошення можливих символів для лотерейних барабанів
 const symbols = ["🍒", "🍊", "🍇", "🍓", "🍉", "🍋", "🍎", "🍏"];
+
+const button = document.getElementById("my-button");
+const buttonWidth = button.offsetWidth;
+const buttonHeight = button.offsetHeight;
+const maxDistance = 100; // Максимальна відстань на яку кнопка буде віддалятись від курсору
+
+button.addEventListener("mousemove", (e) => {
+  const cursorX = e.clientX;
+  const cursorY = e.clientY;
+
+  // Визначаємо відстань кнопки до курсора
+  const buttonRect = button.getBoundingClientRect();
+  const buttonX = buttonRect.left + buttonWidth;
+  const buttonY = buttonRect.top + buttonHeight;
+  const distanceX = cursorX - buttonX;
+  const distanceY = cursorY - buttonY;
+
+  // Обчислюємо зсув кнопки
+  let translateX = 0;
+  let translateY = 0;
+
+  if (Math.abs(distanceX) < maxDistance) {
+    translateX = (-distanceX / maxDistance) * maxDistance;
+  } else {
+    translateX = distanceX < 0 ? maxDistance : -maxDistance;
+  }
+
+  if (Math.abs(distanceY) < maxDistance) {
+    translateY = (-distanceY / maxDistance) * maxDistance;
+  } else {
+    translateY = distanceY < 0 ? maxDistance : -maxDistance;
+  }
+
+  // Застосовуємо зсув до кнопки
+  button.style.transform = `translate(${translateX}px, ${translateY}px)`;
+});
